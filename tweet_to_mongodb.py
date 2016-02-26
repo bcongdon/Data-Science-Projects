cd018294e516c0521e551cdfc4844ea09f3b7455
Benjamin Congdon <bencongdon96@gmail.com>
2016-02-25 20:43:54 -0600

    Allows placing tweet data into mongodb

---
+++ tweet_to_mongodb.py
@@ -0,0 +1,18 @@
+from pymongo import MongoClient
+import json
+
+FILE_NAME = "output.json"
+
+if __name__ == "__main__":
+	client = MongoClient('localhost',27017)
+	db = client['twitter_db']
+	collection = db['twitter_collection']
+	num_lines = sum(1 for line in open(FILE_NAME))
+	with open(FILE_NAME,'r') as f:
+		x = 0
+		for line in f:
+			tweet = json.loads(line)
+			collection.insert(tweet)
+			x+=1
+			if x % 1000 == 0:
+				print str(x) + " out of " + num_lines
