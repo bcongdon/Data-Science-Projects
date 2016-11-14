630c107a70db2b16bea866ec5d90dc876708f287
Benjamin Congdon <bencongdon96@gmail.com>
2016-11-14 10:12:49 -0600

    Density plots for ride times

--- uber/uber.r
+++ uber/uber.r
@@ -1,7 +1,18 @@
 library(ggplot2)
+library(scales)
 
 data = read.csv("./uber-raw-data-apr14.csv")
 head(data)
+
+datetimes <- strptime(data$Date.Time, format="%m/%d/%Y %H:%M:%S")
+times <- strptime(strftime(datetimes, '%X'), format='%X')
+data$just_times = times
+dens <- density(as.numeric(data$Date.Time))
+plot(dens)
+
+ggplot(data) +
+  geom_density(aes(x=just_times), bw="nrd0") +
+  scale_x_datetime(labels=date_format("%H"), date_breaks="2 hour")
 
 min_Lat <- 40.6
 max_Lat <- 40.9
